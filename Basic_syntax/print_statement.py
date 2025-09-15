48eef6f93c6c930a01f2c40487bb5e633c5516d4
Dhrubo <117842503+DhruboChando@users.noreply.github.com>
2025-09-15 09:15:16 +0600

    Create print_statement.py

---
+++ Basic_syntax/print_statement.py
@@ -0,0 +1,10 @@
+'''
+    Topic: Python print statement
+    Author: Anup Chando Dhrubo
+'''
+
+# Print statement with single quote
+print ('hello world')
+
+# Print statement with double quote
+print ("I am Anup Chando Dhrubo")
